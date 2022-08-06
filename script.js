94cf623dd139f3dd4d85fd6fbf4f10d29815280a
Mohammed Sameer Ahmed <sameerahmed976@gmail.com>
2022-08-06 11:10:30 +0530

    added menu-icon

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuIconBtn = document.querySelector(".menu-icon");
+
+menuIconBtn.addEventListener("click", () => {
+  //   console.log("click");
+  menuIconBtn.classList.toggle("active");
+});
